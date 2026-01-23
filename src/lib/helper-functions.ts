22515cd4920e67a587a5567d24eac6ace2598e85
AndyVeversHill <andrew.vevers.hill@icloud.com>
2026-01-24 00:10:20 +0800

    build app global store for updating local, global and db changes for views

--- src/lib/helper-functions.ts
+++ src/lib/helper-functions.ts
@@ -58,7 +58,7 @@ export function translateFiltersState(
 
 // trnsform BE view data to table readable data
 export function applyViewToTableState(
-  activeView:
+  view:
     | {
         sorting?: SortRule[] | null;
         filters?: FilterState[] | null;
@@ -66,55 +66,31 @@ export function applyViewToTableState(
       }
     | null
     | undefined,
-  callbacks: {
-    onSortingChange: (
-      updater: SortingState | ((prev: SortingState) => SortingState),
-    ) => void;
-    onColumnFiltersChange: (
-      updater:
-        | ColumnFiltersState
-        | ((prev: ColumnFiltersState) => ColumnFiltersState),
-    ) => void;
-    onColumnVisibilityChange: (
-      updater: VisibilityState | ((prev: VisibilityState) => VisibilityState),
-    ) => void;
+  {
+    onSortingChange,
+    onColumnFiltersChange,
+    onColumnVisibilityChange,
+  }: {
+    onSortingChange: (state: SortingState) => void;
+    onColumnFiltersChange: (state: ColumnFiltersState) => void;
+    onColumnVisibilityChange: (state: VisibilityState) => void;
   },
 ): void {
-  if (!activeView) {
-    callbacks.onSortingChange([]);
-    callbacks.onColumnFiltersChange([]);
-    callbacks.onColumnVisibilityChange({});
-    return;
-  }
-
-  // Sorting
-  const sortingState: SortingState =
-    activeView.sorting?.map((sort) => ({
-      id: sort.columnId,
-      desc: sort.direction === "desc",
-    })) ?? [];
-
-  callbacks.onSortingChange(sortingState);
-
-  // Filters
-  const filtersState: ColumnFiltersState =
-    activeView.filters?.map((filter) => ({
-      id: filter.columnId,
-      value: {
-        operator: filter.operator,
-        value: filter.value as unknown,
-      },
-    })) ?? [];
-
-  callbacks.onColumnFiltersChange(filtersState);
-
-  const visibilityState: VisibilityState = {};
-
-  for (const colId of activeView.hidden ?? []) {
-    visibilityState[colId] = false;
-  }
-
-  callbacks.onColumnVisibilityChange(visibilityState);
+  onSortingChange(
+    view?.sorting?.map((s) => ({
+      id: s.columnId,
+      desc: s.direction === "desc",
+    })) ?? [],
+  );
+  onColumnFiltersChange(
+    view?.filters?.map((f) => ({
+      id: f.columnId,
+      value: { operator: f.operator, value: f.value },
+    })) ?? [],
+  );
+  onColumnVisibilityChange(
+    view?.hidden?.reduce((acc, id) => ({ ...acc, [id]: false }), {}) ?? {},
+  );
 }
 
 // transforms the sorts inside the form so we can send them to update the views
